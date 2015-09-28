f19963d54adaf78ddda8bbcad755ea966ddb0a79
Musta Rohman <mustarohman@gmail.com>
2015-09-28 15:43:23 +0100

    User interface and number input

---
+++ app/src/main/java/com/example/mustarohman/integercalculator/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.mustarohman.integercalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private String inputNumber = "0";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickNumberBtn(View view) {
+
+        if (inputNumber.equals("0")){
+            inputNumber = "";
+        };
+
+        Button numberBtn = (Button) view;
+        inputNumber+= numberBtn.getText().toString();
+
+        updateNumberText();
+
+    }
+
+    public void updateNumberText(){
+
+        textView.setText(inputNumber);
+
+    }
+
+    public void onClickCBtn(View view) {
+
+        inputNumber = "";
+        updateNumberText();
+
+    }
+}
